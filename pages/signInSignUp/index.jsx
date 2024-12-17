3efe8d1bffdace7c7343750042abcaf72c9c4a59
Kidus Mesfin <kidus0237@gmail.com>
2024-12-17 10:18:22 +0300

    fixed tailwind bug

--- pages/signInSignUp/index.jsx
+++ pages/signInSignUp/index.jsx
@@ -1,7 +1,7 @@
 export default function SignIn_SignUp(){
     return(
         <div>
-            <h1>Bitch nigga</h1>
+            <h1 className="text-3xl font-bold text-teal-500 underlinetext-teal-400">Bitch nigga</h1>
         </div>
     )
 }
