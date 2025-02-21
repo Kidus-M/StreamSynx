d0c5345cd51bd62e79012dfec7c374374ced50f6
Kidus Mesfin <kidus0237@gmail.com>
2025-02-21 17:14:20 +0300

    fixed some of the history part

--- pages/history/index.jsx
+++ pages/history/index.jsx
@@ -34,7 +34,8 @@ const HistoryPage = () => {
   return (
     <div className="text-secondary bg-primary min-h-screen">
       <NavBar />
-      <main className="p-6">
+      <main className="p-6 mt-24">
+        <div className="flex align-middle  justify-between">
         <h1 className="text-2xl font-bold mb-6">Watch History</h1>
         <button
           onClick={clearHistory}
@@ -42,6 +43,7 @@ const HistoryPage = () => {
         >
           Clear History
         </button>
+        </div>
         <div>
           <h2 className="text-xl font-bold mb-4">Movies</h2>
           <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
