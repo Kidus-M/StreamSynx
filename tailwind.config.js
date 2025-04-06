ced955f959ba06af667d9821eef92760aed3b3b9
Kidus Mesfin <kidus0237@gmail.com>
2025-04-06 17:36:11 +0300

    better colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#121212",
-        secondary: "#282828",
-        tertiary: "#DAA520",
+        primary: "#121212", // Near Black
+        secondary: "#282828", // Dark Gray
+        accent: "#DAA520", // Goldenrod
+        accentcopper: "#B87333", // Copper
+        accentsoftgold: "#F4BF4F", // Softer Gold
+        textprimary: "#EAEAEA", // Very Light Gray
+        textsecondary: "#A0A0A0", // Medium Gray
       },
       fontFamily: {
         'dm-display': ['"DM Serif Display"', 'serif'],
