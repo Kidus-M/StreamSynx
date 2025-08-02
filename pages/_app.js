792114f0f1e1bf75fa653583141fc4f1c46067a4
kidus-m <kidus0237@gmail.com>
2025-08-02 12:09:17 +0300

    head for hist  page

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,14 @@ export default function App({ Component, pageProps }) {
         {/* Add other sizes if needed, e.g., 16x16, 32x32 */}
         {/* Theme Color for browser UI */}
         <meta name="theme-color" content="#121212" /> {/* Your primary dark color */}
+
+          <title>StreamSynx | Discover Movies & TV Shows</title>
+          <meta name="description" content="Discover trending, popular, and top-rated movies and TV shows. Track your watch history, create a watchlist, and share recommendations with friends on StreamSynx." />
+          <meta name="keywords" content="movies, tv shows, trending, popular, watchlist, recommendations, stream, watch party" />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+
       </Head>
   <Component {...pageProps} /></>
 }
